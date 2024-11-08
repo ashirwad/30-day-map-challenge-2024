643f8cc36c1bda5ed8d7c42f33dd0950c41fcbd7
ashirwad <ashirwad1992@gmail.com>
2024-11-07 21:27:34 -0600

    Pad time col with zeroes

--- code/day04-hexagons.R
+++ code/day04-hexagons.R
@@ -1,7 +1,8 @@
 ames_pd_charges <- readr::read_csv("data/ames-pd-sep-2024-charges.csv") |>
   janitor::clean_names() |>
   dplyr::mutate(
-    location_of_arrest = glue::glue("{location_of_arrest}, Ames, IA")
+    location_of_arrest = glue::glue("{location_of_arrest}, Ames, IA"),
+    time = stringr::str_pad(time, width = 4, pad = "0")
   ) |>
   tidygeocoder::geocode(location_of_arrest, method = "arcgis")
 
