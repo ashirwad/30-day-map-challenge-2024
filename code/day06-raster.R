b42036bd12d8fe25740117031c8c785fbd0ea706
ashirwad <ashirwad1992@gmail.com>
2024-11-08 21:59:49 -0600

    Prettify layer name in elevation raster map

--- code/day06-raster.R
+++ code/day06-raster.R
@@ -5,7 +5,10 @@ ames_elevation <- elevatr::get_elev_raster(
   locations = ames_boundary, z = 9, clip = "locations"
 )
 
-ames_elevation_map <- mapview::mapview(ames_elevation)
+ames_elevation_map <- mapview::mapview(
+  ames_elevation,
+  layer.name = "Elevation (meters)"
+)
 
 htmlwidgets::saveWidget(
   ames_elevation_map@map, here::here("maps", "day06-raster.html"),
