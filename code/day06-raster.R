4e7794dacf67890ed976996382404c1323e29880
ashirwad <ashirwad1992@gmail.com>
2024-11-08 21:38:18 -0600

    Add code to create elevation raster map for day 06

---
+++ code/day06-raster.R
@@ -0,0 +1,13 @@
+ames_boundary <- tigris::places(state = "IA", cb = TRUE, year = 2020) |>
+  dplyr::filter(NAME == "Ames")
+
+ames_elevation <- elevatr::get_elev_raster(
+  locations = ames_boundary, z = 9, clip = "locations"
+)
+
+ames_elevation_map <- mapview::mapview(ames_elevation)
+
+htmlwidgets::saveWidget(
+  ames_elevation_map@map, here::here("maps", "day06-raster.html"),
+  selfcontained = FALSE
+)
